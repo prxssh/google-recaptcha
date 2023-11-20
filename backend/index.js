85d3919cada437b8e3bf1bced5460d6bbaf7baeb
prxssh <prxxshu@gmail.com>
2023-11-20 10:38:53 +0530

    feat: setup backend api for captcha verification

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors')
+const app = express();
+
+app.use(express.json());
+const corsOptions = { credentials: true };
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/ping', async(req, res) => {
+    res.status(200).json({"message": "pong"})
+})
+
+app.post('/verify-recaptcha', async (req, res) => {
+  const token = req.body.token;
+  const secretKey = process.env.SECRET_KEY
+  const url = 'https://www.google.com/recaptcha/api/siteverify';
+    console.log("called")
+
+  try {
+    const response = await axios.post(url, null, {
+      params: {
+        secret: secretKey,
+        response: token
+      }
+    });
+
+      console.log(response.data)
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(3001, () => console.log('Server running on port 3001'));
