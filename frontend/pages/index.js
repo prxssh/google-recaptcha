a0ae97a7a647a9050be67398a0d351c58354e28f
prxssh <prxxshu@gmail.com>
2023-11-20 10:55:36 +0530

    feat: setup new api keys

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,20 +6,18 @@ const HomePage = () => {
   const recaptchaRef = useRef(null)
   const [isVerified, setIsVerified] = useState(false)
 
-    async function verifyCaptcha( token ) {
-        const res = await axios.post('http://localhost:3001/verify-recaptcha', { token: token })
+    async function verifyCaptcha(token) {
+        const res = await axios.post('http://recaptcha-test-4rox.vercel.app//verify-recaptcha', { token: token })
         if (res.data.success) {
-            alert("Captcha Successful!")
             setIsVerified(true)
-        } else {
-            alert("You're a Robot")
+            alert("Captcha Successful!")
         }
     }
 
   return (
         <>
           <ReCAPTCHA
-            sitekey="6LeGOhUpAAAAAIW15okgzhSC4g3HpiAODWP6sclP"
+            sitekey="6LdmRBUpAAAAAEssRNk4qMHxhe3_w-nK58BLUfao"
             ref={recaptchaRef}
             onChange={verifyCaptcha}
           />
