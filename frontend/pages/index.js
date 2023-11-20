4f84d3bdc3884d3156bf3844c66afb81b1147bd6
prxssh <prxxshu@gmail.com>
2023-11-20 08:18:37 +0530

    feat: setting up recaptcha frontend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
     <>
-      <p> Recaptcha Test </p>
+  <head>
+    <title>reCAPTCHA demo: Simple page</title>
+    <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
+  </head>
+  <body>
+    <form action="" method="POST">
+      <div class="g-recaptcha" data-sitekey="6Lfd1xQpAAAAAD0aiI-n8Un4SREu6BczNj7aEVri" data-action="LOGIN"></div>
+      <br/>
+          <input type="submit" value="Submit"/>
+    </form>
+  </body>
     </>
   )
 }
